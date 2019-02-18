5896ce6174d9a6e32660396bd5c6b0e7d68c52fb
Sam Kamin <kamin@illinois.edu>
2019-02-18 17:07:15 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 function markdone(cb) {
+   // 'row' refers to the <tr> that includes this 
    var row = cb.closest("tr");
    row.classList.toggle("done");
 }
 
 function addsillytodo() {
     var todolist = $("table");
-    todolist.append("<tr><td>a</td><td>b</td></tr>");
+    todolist.append('<tr><td>a</td><td>b</td><td><input type="checkbox" onclick="markdone(this)"></tr>');
 }
 
 function addtodo() {
@@ -13,5 +14,6 @@ function addtodo() {
     var poms = prompt("how many pomodoros?");
     var todolist = $("table");
     todolist.append("<tr><td>" + todo + "</td>"
-                    + "<td>" + poms + " poms </td></tr>");
+                    + "<td>" + poms + " poms </td>"
+                    + '<td><input type="checkbox" onclick="markdone(this)"></tr>');
 }
